097a718cc3b7883ce7ccb6786980d9aa9a3c7111
Bogdan-Adrian Burciu <59748058+bogdanadrian-burciu@users.noreply.github.com>
2020-06-13 11:22:40 +0300

    Update application.py

--- application.py
+++ application.py
@@ -3,4 +3,20 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello World!"
+    return """Hello World, this was created by cloning https://github.com/Azure-Samples/python-docs-hello-world to https://github.com/bogdanadrian-burciu/Azure-webApp-PythonFlask repo, then creating a new webApp in Azure Portal,
+adding the repo in Azure WebApp Deployment Center.
+Once deployment has completed, switch back to the browser window open to http://<app-name>.azurewebsites.net.
+Guide used: https://docs.microsoft.com/en-us/azure/app-service/containers/quickstart-python?tabs=powershell.
+
+PS C:\Users\boburciu\python-docs-hello-world> nslookup boburciu-webapp.azurewebsites.net
+Server:  abuhins1.buh.is.keysight.com
+Address:  10.25.41.140
+
+Non-authoritative answer:
+Name:    waws-prod-db3-141.cloudapp.net
+Address:  13.69.228.5
+Aliases:  boburciu-webapp.azurewebsites.net
+          waws-prod-db3-141.sip.azurewebsites.windows.net
+
+PS C:\Users\boburciu\python-docs-hello-world>
+"""
